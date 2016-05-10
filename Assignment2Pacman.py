cde3a97f4f7a31c480283fc9ab4342ffd838d787
Jonas Scharpf <info@brainelectronics.de>
2016-05-10 21:19:13 +0200

    added some test cases and updated code

--- Assignment2Pacman.py
+++ Assignment2Pacman.py
@@ -54,6 +54,7 @@ Your solution should work on propositional level, i.e. you should not have to
 parse the positions out of the state symbols, but write rules that work on the 
 state directly.
 """
+"""
 testState = [
 '-C_0_0', 'NV_0_1', 'NV_0_2', '-C_0_3',
 'NV_1_0', 'C_1_1', 'NV_1_2', 'C_1_3',
@@ -77,6 +78,20 @@ testState4 = [
 'NV_1_0', 'NV_1_1', 'NV_1_2', 'C_1_3',
 'NV_2_0', 'NV_2_1', 'NV_2_2', 'NV_2_3',
 'NV_3_0', 'NV_3_1', 'NV_3_2', 'NV_3_3']
+"""
+someTestStates = [
+['NV_0_0', 'NV_0_1', 'NV_0_2', 'NV_0_3', 'NV_1_0', 'C_1_1', 'NV_1_2', 'NV_1_3', 'NV_2_0', 'NV_2_1', 'C_2_2', 'NV_2_3', 'NV_3_0', 'NV_3_1', 'NV_3_2', 'NV_3_3'],
+['NV_0_0', 'NV_0_1', 'NV_0_2', 'NV_0_3', 'NV_1_0', 'NV_1_1', 'NV_1_2', 'NV_1_3', 'NV_2_1', 'NV_2_0', 'NV_2_2', 'NV_2_3', 'NV_3_0', 'NV_3_2', 'NV_3_1', 'NV_3_3'],
+['C_0_0', 'NV_0_1', 'NV_0_2', 'NV_0_3', 'NV_1_0', 'NV_1_1', 'NV_1_2', 'NV_1_3', 'NV_2_1', 'NV_2_0', 'NV_2_2', 'NV_2_3', 'NV_3_0', 'NV_3_2', 'NV_3_1', 'NV_3_3'],
+['-C_0_0', 'NV_0_1', 'NV_0_2', 'NV_0_3', 'NV_1_0', 'NV_1_1', 'NV_1_2', 'C_1_3', 'NV_2_1', 'NV_2_0', 'NV_2_2', 'NV_2_3', 'NV_3_0', 'NV_3_2', 'NV_3_1', 'NV_3_3'],
+['NV_0_0', '-C_0_1', 'NV_0_2', 'NV_0_3', '-C_1_0', 'C_1_1', 'NV_1_2', 'NV_1_3', '-C_2_1', 'NV_2_0', 'NV_2_2', 'NV_2_3', 'NV_3_0', 'NV_3_2', 'NV_3_1', '-C_3_3'],
+['NV_0_0', 'NV_0_1', 'C_0_2', 'NV_0_3', '-C_1_0', '-C_1_1', '-C_1_2', 'C_1_3', 'NV_2_0', 'NV_2_1', '-C_2_2', '-C_2_3', '-C_3_0', '-C_3_1', '-C_3_2', '-C_3_3'],
+['-C_0_0', 'NV_0_1', '-C_0_2', '-C_0_3', 'C_1_0', '-C_1_1', '-C_1_2', '-C_1_3', 'NV_2_0', 'C_2_1', '-C_2_2', '-C_2_3', 'C_3_0', 'NV_3_1', '-C_3_2', '-C_3_3'],
+['-C_0_0', 'NV_0_1', '-C_0_2', '-C_0_3', 'NV_1_0', 'NV_1_1', '-C_1_2', 'NV_1_3', 'C_2_0', 'NV_2_1', 'C_2_2', 'NV_2_3', '-C_3_0', 'NV_3_1', '-C_3_2', '-C_3_3'],
+['-C_0_0', '-C_0_1', 'NV_0_2', '-C_0_3', 'NV_1_0', 'C_1_1', '-C_1_2', 'NV_1_3', 'C_2_0', 'NV_2_1', 'NV_2_2', '-C_2_3', '-C_3_0', 'C_3_1', '-C_3_2', '-C_3_3'],
+['NV_0_0', 'C_0_1', 'NV_0_2', '-C_0_3', 'C_1_0', 'NV_1_1', '-C_1_2', 'NV_1_3', '-C_2_0', '-C_2_1', 'NV_2_2', 'NV_2_3', 'NV_3_0', '-C_3_1', 'NV_3_2', '-C_3_3'],
+['-C_0_0', 'C_0_1', '-C_0_2', '-C_0_3', 'C_1_0', 'NV_1_1', 'NV_1_2', '-C_1_3', '-C_2_0', 'NV_2_1', 'NV_2_2', 'NV_2_3', 'NV_3_0', '-C_3_1', '-C_3_2', '-C_3_3']
+]
 
 import pprint
 def show_state(state, ghost_pos=None):
@@ -161,29 +176,13 @@ def is_1_2_safe(state):
 			else:	# no 'C' or '0'
 				theInsertValue = 0
 				pass
-
-			# currentPosition = lineCounter*4+elementCounter
-			# topPosition = (lineCounter-1)*4+elementCounter
-			# bottomPosition = (lineCounter+1)*4+elementCounter
-			# leftPosition = lineCounter*4+(elementCounter-1)
-			# rightPosition = lineCounter*4+(elementCounter+1)
-
-			# newDict[currentPosition] =  -999
-
-			# if (lineCounter-1) >= 0:	# there is a line above available
-			# 	newDict[topPosition] += theInsertValue
-			# if (lineCounter+1) < 4:		# there is a line underneath available
-			# 	newDict[bottomPosition] += theInsertValue
-			# if (elementCounter-1) >= 0:	# there is a left row available
-			# 	newDict[leftPosition] += theInsertValue
-			# if (elementCounter+1) < 4:	# there is a right row available
-			# 	newDict[rightPosition] += theInsertValue
-
-
+    # '''
+    # debug
 	for x in range(16):
 		if x%4 == 0:
 			print "\n",
 		print "%s," %newDict[x],
+    # '''
 
 	print "\n"
 	ghostList = [element for element, pos in newDict.items() if pos > 0]
@@ -192,26 +191,24 @@ def is_1_2_safe(state):
 
 	ghostPos = [(seems/4, ((seems+1)%4)-1) for seems in ghostList ]
 	print "ghostPos %s" %ghostPos
-	# return 0
 
+    # a ghost at (1,2) and only one ghost detected
+    # OR
+    # a ghost at (1,2) and the most likely is at (1,2)
 	if (1,2) in ghostPos and (len(ghostPos) == 1 or ((mostSure/4, ((mostSure+1)%4)-1) == (1,2))):
 		return 'Ghost_1_2'	# Ghost_1_2 == a ghost
+
+    # there are several ghots BUT none at (1,2)
 	elif (1,2) not in ghostPos:
 		return 'Safe_1_2'	# Safe_1_2 == no ghost
+
+    # anything else
 	else:
 		return 'Unsafe_1_2'	# Unsafe_1_2 == maybe a ghost
 
 if __name__ == '__main__':
-	print is_1_2_safe(state=testState)
-	print "\n"
-
-	print is_1_2_safe(state=testState2)
-	print "\n"
-
-	print is_1_2_safe(state=testState3)
-	print "\n"
-
-	print is_1_2_safe(state=testState4)
-	print "\n"
+    for tests in someTestStates:
+        print is_1_2_safe(state=tests)
+        print "\n\n\n"
 
 

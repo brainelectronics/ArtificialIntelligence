8ad4b1d2791163d2c747b41661c4b3bfe4f2365f
Jonas Scharpf <jonas.scharpf@tum.de>
2016-05-11 13:37:53 +0200

    improved to 16/16

--- Assignment2Pacman.py
+++ Assignment2Pacman.py
@@ -38,10 +38,10 @@ chill, and NV_i_j if Pacman hasn't been there yet. This is an example state:
 The state can be visualized rudimentarily with the show_state function given 
 below - for this example state it looks like this:
 
-[['0', '.', '.', '0'],	[(0,0), (0,1), (0,2), (0,3)],
- ['.', 'C', 'G', 'C'],	[(1,0), (1,1),  ?G? , (1,3)],
- ['0', '.', 'C', '0'],	[(2,0), (2,1), (2,2), (2,3)],
- ['0', '.', '.', '0']]	[(3,0), (3,1), (3,2), (3,3)],
+[['0', '.', '.', '0'],  [(0,0), (0,1), (0,2), (0,3)],
+ ['.', 'C', 'G', 'C'],  [(1,0), (1,1),  ?G? , (1,3)],
+ ['0', '.', 'C', '0'],  [(2,0), (2,1), (2,2), (2,3)],
+ ['0', '.', '.', '0']]  [(3,0), (3,1), (3,2), (3,3)],
 
 Please provide a function that, based on this state description, returns one of 
 the labels Safe_1_2, Unsafe_1_2 or Ghost_1_2, which tell if there cannot be a 
@@ -105,22 +105,22 @@ someTestStates = [
 
 import pprint
 def show_state(state, ghost_pos=None):
-	"""
-	Show the state graphically, and the position of the ghost, if it is known
-	"""
-	chars = {'C': 'C', 
-			 '-C': '0',
-			 'NV': '.'}
-	
-	ss = [range(4) for _ in range(4)]
-	for s in state:
-		c, i, j = s.split('_')
-		ss[int(i)][int(j)] = chars[c]
-	if ghost_pos:
-		ss[ghost_pos[0]][ghost_pos[1]] = 'G'
+    """
+    Show the state graphically, and the position of the ghost, if it is known
+    """
+    chars = {'C': 'C', 
+             '-C': '0',
+             'NV': '.'}
+    
+    ss = [range(4) for _ in range(4)]
+    for s in state:
+        c, i, j = s.split('_')
+        ss[int(i)][int(j)] = chars[c]
+    if ghost_pos:
+        ss[ghost_pos[0]][ghost_pos[1]] = 'G'
 
-	pprint.pprint(ss)
-	return ss
+    pprint.pprint(ss)
+    return ss
 
 """
 show_state returns:
@@ -132,94 +132,96 @@ show_state returns:
 """
 
 def is_1_2_safe(state):
-	"""
-	a function that determines in a propositional way if it safe to move to 
-	the (1, 2) square.
+    """
+    a function that determines in a propositional way if it safe to move to 
+    the (1, 2) square.
 
-	Safe_1_2 == no ghost
-	Unsafe_1_2 == maybe a ghost
-	Ghost_1_2 == a ghost
-	"""
-	# orginal
-	# if 1:
-	# 	return 'Safe_1_2'	# Safe_1_2 == no ghost
-	# elif 2:
-	# 	return 'Ghost_1_2'	# Ghost_1_2 == a ghost
-	# else:
-	# 	return 'Unsafe_1_2'	# Unsafe_1_2 == maybe a ghost
+    Safe_1_2 == no ghost
+    Unsafe_1_2 == maybe a ghost
+    Ghost_1_2 == a ghost
+    """
+    # orginal
+    # if 1:
+    #   return 'Safe_1_2'   # Safe_1_2 == no ghost
+    # elif 2:
+    #   return 'Ghost_1_2'  # Ghost_1_2 == a ghost
+    # else:
+    #   return 'Unsafe_1_2' # Unsafe_1_2 == maybe a ghost
 
-	newDict = dict.fromkeys(range(0,16), 0)	# generate new dict with keys 0-16 and values 0
-	chillyList = []
-	problemMatrix = show_state(state=state, ghost_pos=None)
-	for lineCounter, line in enumerate(problemMatrix):
-		for elementCounter, element in enumerate(line):
-			currentPosition = lineCounter*4+elementCounter
-			topPosition = (lineCounter-1)*4+elementCounter
-			bottomPosition = (lineCounter+1)*4+elementCounter
-			leftPosition = lineCounter*4+(elementCounter-1)
-			rightPosition = lineCounter*4+(elementCounter+1)
+    newDict = dict.fromkeys(range(0,16), 0) # generate new dict with keys 0-16 and values 0
+    chillyList = []
+    problemMatrix = show_state(state=state, ghost_pos=None)
+    for lineCounter, line in enumerate(problemMatrix):
+        for elementCounter, element in enumerate(line):
+            currentPosition = lineCounter*4+elementCounter
+            topPosition = (lineCounter-1)*4+elementCounter
+            bottomPosition = (lineCounter+1)*4+elementCounter
+            leftPosition = lineCounter*4+(elementCounter-1)
+            rightPosition = lineCounter*4+(elementCounter+1)
 
-			if 'C' in element:
-				theInsertValue = 10
-				newDict[currentPosition] =  -999
+            if 'C' in element:
+                theInsertValue = 10
+                newDict[currentPosition] =  -999
 
-				if (lineCounter-1) >= 0:	# there is a line above available
-					newDict[topPosition] += theInsertValue
-				if (lineCounter+1) < 4:		# there is a line underneath available
-					newDict[bottomPosition] += theInsertValue
-				if (elementCounter-1) >= 0:	# there is a left row available
-					newDict[leftPosition] += theInsertValue
-				if (elementCounter+1) < 4:	# there is a right row available
-					newDict[rightPosition] += theInsertValue
-			elif '0' in element:
-				theInsertValue = -10
-				newDict[currentPosition] =  -555
+                if (lineCounter-1) >= 0:    # there is a line above available
+                    newDict[topPosition] += theInsertValue
+                if (lineCounter+1) < 4:     # there is a line underneath available
+                    newDict[bottomPosition] += theInsertValue
+                if (elementCounter-1) >= 0: # there is a left row available
+                    newDict[leftPosition] += theInsertValue
+                if (elementCounter+1) < 4:  # there is a right row available
+                    newDict[rightPosition] += theInsertValue
+            elif '0' in element:
+                theInsertValue = -10
+                newDict[currentPosition] =  -555
 
-				if (lineCounter-1) >= 0:	# there is a line above available
-					newDict[topPosition] += theInsertValue
-				if (lineCounter+1) < 4:		# there is a line underneath available
-					newDict[bottomPosition] += theInsertValue
-				if (elementCounter-1) >= 0:	# there is a left row available
-					newDict[leftPosition] += theInsertValue
-				if (elementCounter+1) < 4:	# there is a right row available
-					newDict[rightPosition] += theInsertValue
-			else:	# no 'C' or '0'
-				theInsertValue = 0
-				pass
-	# '''
-	# debug
-	for x in range(16):
-		if x%4 == 0:
-			print "\n",
-		print "%s," %newDict[x],
-	# '''
+                if (lineCounter-1) >= 0:    # there is a line above available
+                    newDict[topPosition] += theInsertValue
+                if (lineCounter+1) < 4:     # there is a line underneath available
+                    newDict[bottomPosition] += theInsertValue
+                if (elementCounter-1) >= 0: # there is a left row available
+                    newDict[leftPosition] += theInsertValue
+                if (elementCounter+1) < 4:  # there is a right row available
+                    newDict[rightPosition] += theInsertValue
+            else:   # no 'C' or '0'
+                theInsertValue = 0
+    # '''
+    # debug
+    for x in range(16):
+        if x%4 == 0:
+            print "\n",
+        print "%s," %newDict[x],
+    # '''
 
-	print "\n"
-	ghostList = [element for element, pos in newDict.items() if pos > 0]
-	print "ghosts at id %s" %ghostList
-	mostSure = max(newDict, key=newDict.get)
-	sameSure = [ele for ele, val in newDict.items() if val == newDict[mostSure]]
+    print "\n"
+    ghostList = [element for element, pos in newDict.items() if pos > 0]
+    print "ghosts at id %s" %ghostList
+    mostSure = max(newDict, key=newDict.get)
+    sameSure = [ele for ele, val in newDict.items() if val == newDict[mostSure]]
 
-	ghostPos = [(seems/4, ((seems+1)%4)-1) for seems in ghostList ]
-	print "ghostPos %s" %ghostPos
+    ghostPos = [(seems/4, ((seems+1)%4)-1) for seems in ghostList ]
+    print "ghostPos %s" %ghostPos
 
-	# a ghost at (1,2) and only one ghost detected
-	# OR
-	# a ghost at (1,2) and the most likely is at (1,2)
-	if (1,2) in ghostPos and len(sameSure)==1 and (len(ghostPos) == 1 or ((mostSure/4, ((mostSure+1)%4)-1) == (1,2))):
-		return 'Ghost_1_2'	# Ghost_1_2 == a ghost
+    # a ghost at (1,2) and only one ghost detected
+    # OR
+    # a ghost at (1,2) and the most likely is at (1,2)
+    if (1,2) in ghostPos and len(sameSure)==1 and (len(ghostPos) == 1 or ((mostSure/4, ((mostSure+1)%4)-1) == (1,2))):
+        return 'Ghost_1_2'  # Ghost_1_2 == a ghost
 
-	# there are several ghots BUT none at (1,2)
-	elif (1,2) not in ghostPos:
-		return 'Safe_1_2'	# Safe_1_2 == no ghost
+    # there are several ghots BUT none at (1,2)
+    elif (1,2) not in ghostPos and len(ghostPos) > 0:
+        return 'Safe_1_2'   # Safe_1_2 == no ghost
+        
+    elif (1,2) not in ghostPos and len(ghostPos) == 0:
+        return 'Unsafe_1_2'   # Safe_1_2 == no ghost
 
-	# anything else
-	else:
-		return 'Unsafe_1_2'	# Unsafe_1_2 == maybe a ghost
+    # anything else
+    else:
+        return 'Unsafe_1_2' # Unsafe_1_2 == maybe a ghost
 
 if __name__ == '__main__':
-	for tests in someTestStates:
-		print is_1_2_safe(state=tests)
-		print "\n\n\n"
+    for tests in someTestStates:
+        print is_1_2_safe(state=tests)
+        print "\n\n\n"
 
 
